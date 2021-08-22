628ddbd00687afc258f98a14ae8f10a2e9c1f592
Daniel Campoverde <alx@sillybytes.net>
2021-08-22 16:59:59 -0500

    Pandoc types

--- src/Generation.hs
+++ src/Generation.hs
@@ -1,19 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Generation
-    (writeMarkdown
+    ( readBook
     ) where
 
 import           Data.Bool    (bool)
 import           Data.Maybe   (isJust)
 import           Data.Text as T
 import qualified Data.Text.IO as TIO
+<<<<<<< Updated upstream
 import Data.Time.Format
 import Data.Time
+=======
+import Text.Pandoc
+import Data.Map.Strict
+import Data.Time.Clock (UTCTime)
+import Data.Time.Format (formatTime, defaultTimeLocale)
+>>>>>>> Stashed changes
 
 import Types
 
+-- readBooks :: PandocMonad m => [Book] -> m Pandoc
+-- readBooks books = undefined
 
+<<<<<<< Updated upstream
 writeMarkdown :: FilePath -> [Book] -> IO ()
 writeMarkdown fp books = do
 
@@ -57,3 +67,57 @@ header time = "---\n"
 
 -- prepend :: Text -> Text -> Text
 -- prepend a b = a <> b
+=======
+
+readBook :: PandocMonad m => Book -> m Pandoc
+readBook book = do
+    time <- getCurrentTime
+    pure $ Pandoc (meta time) [(Para [Str "test"])]
+    where
+        meta time = Meta $ fromList
+            [ ("title",  MetaString "Highlights & Notes")
+            , ("author", MetaString "kobonotes.sillybytes.net")
+            , ("date",   MetaString $ yearMonthFormatter time)
+            ]
+
+        yearMonthFormatter :: UTCTime -> Text
+        yearMonthFormatter = pack . formatTime defaultTimeLocale "%B %Y"
+
+
+-- writeMarkdown :: FilePath -> [Book] -> IO ()
+-- writeMarkdown fp books = TIO.writeFile fp
+--     $   header
+--     <> T.concat (bookEntry <$> books)
+
+-- bookEntry :: Book -> Text
+-- bookEntry (Book title author highlights)
+--     =  "\n\n\n"
+--     <> "# "  <> title  <> "\n"
+--     <> "## " <> author <> "\n\n"
+--     <> T.concat (highlightEntry <$> highlights)
+--     <> "-------"
+
+-- highlightEntry :: Highlight -> Text
+-- highlightEntry (Highlight highl note date)
+--     =  "\n"
+--     <> (stripEnd . T.unlines) (("> " <>) . stripStart <$> T.lines highl)
+--     <> "   *" <> date <> "*" <> "\n"
+--     <> maybe "" ("\n| " <>) note''
+--     <> "\n"
+--     where note' = strip <$> note
+--           note'' = case note' of
+--                         Just    "" -> Nothing
+--                         Just    n  -> Just n
+--                         Nothing    -> Nothing
+
+-- header :: Text
+-- header = "---\n"
+--        <> "title: Kobo highlights & notes\n"
+--        <> "...\n"
+--        <> "\n"
+--        <> "\\tableofcontents\n"
+
+
+-- -- prepend :: Text -> Text -> Text
+-- -- prepend a b = a <> b
+>>>>>>> Stashed changes
--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Monad (forM_)
@@ -8,9 +10,36 @@ import Types
 import Extraction
 import Generation
 
+import Data.Default
+
+import Text.Pandoc
+
+someBook = Book "titulo" "autor" []
+
 main :: IO ()
 main = do
-    (dbFile:outFile:_) <- getArgs
-    books <- extractBooks dbFile
-    writeMarkdown outFile books
-    putStrLn $ "Extracted highlights & notes for " <> show (length books) <> " books"
+    pandocEither <- runIO $ readBook someBook
+
+    case pandocEither of
+        Left err -> print err
+        Right pandoc -> do
+            print pandoc
+
+            templateT <- runIO $ getTemplate "template/template.html"
+            -- print (templateT :: _)
+            let templateEither = fmap (compileTemplate "") templateT
+            print templateEither
+
+
+            -- case templateEither of
+            --     Left err -> print err
+            --     Right template -> do
+            --         print template
+
+            -- html <- runIO $ writeHtml5String (def {writerTemplate = Just }) pandoc
+            -- print html
+
+    -- (dbFile:outFile:_) <- getArgs
+    -- books <- extractBooks dbFile
+    -- writeMarkdown outFile books
+    -- putStrLn $ "Extracted highlights & notes for " <> show (length books) <> " books"
